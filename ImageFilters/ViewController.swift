586f16f3bcfb6249dac371a2dd54234fa5fecd02
Vlad Eliseev <freshymanchan@gmail.com>
2019-09-05 13:53:58 +1000

    Initial Commit

---
+++ ImageFilters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilters
+//
+//  Created by Vlad Eliseev on 05/09/2019.
+//  Copyright © 2019 Vlad Eliseev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
